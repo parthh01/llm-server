47176bddb2cf9a5ac59df581eb96b9e05fcdf42a
Parth Bhagat <parthb@nyu.edu>
2024-01-15 20:07:54 -0800

    add paths, put litellm behind nginx

--- scripts/openai_completions.py
+++ scripts/openai_completions.py
@@ -1,7 +1,7 @@
 import openai
 from openai import OpenAI
 
-client = OpenAI(base_url='http://34.41.199.129',api_key='sk-6786') 
+client = OpenAI(base_url='http://localhost/litellm',api_key='sk-6786') 
 
 
-print(client.chat.completions.create(model="ollama/phi", messages=[{"role":"user", "content":"Hey!"}]))
+print(client.chat.completions.create(model="ollama/phi", messages=[{"role":"user", "content":"What is the capital of the US?"}]))
