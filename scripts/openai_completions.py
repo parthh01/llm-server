095292e04e2169fbaeac942022e555272e734da4
Parth Bhagat <parthb@nyu.edu>
2024-01-17 19:57:43 -0800

    make changes

--- scripts/openai_completions.py
+++ scripts/openai_completions.py
@@ -1,7 +1,7 @@
 import openai
 from openai import OpenAI
 
-client = OpenAI(base_url='http://localhost/litellm',api_key='sk-6786') 
+client = OpenAI(base_url='http://34.27.58.60/litellm',api_key='sk-6786') 
 
 
-print(client.chat.completions.create(model="ollama/phi", messages=[{"role":"user", "content":"What is the capital of the US?"}]))
+print(client.chat.completions.create(model="ollama/mixtral", messages=[{"role":"user", "content":"What is the capital of the US?"}]))
