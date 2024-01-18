74de63f4faf62d57a17e0c9ffe63e93b38956ca7
Parth Bhagat <parthb@nyu.edu>
2024-01-17 20:30:16 -0800

    fix startup

--- ollama_startup.sh
+++ ollama_startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 curl http://ollama:11434/api/pull -d '{
+  "name": "mixtral"
+}'
+
+curl http://ollama:11434/api/pull -d '{
   "name": "phi"
 }'
--- scripts/openai_completions.py
+++ scripts/openai_completions.py
@@ -1,7 +1,7 @@
 import openai
 from openai import OpenAI
 
-client = OpenAI(base_url='http://34.27.58.60/litellm',api_key='sk-6786') 
+client = OpenAI(base_url='http://34.42.135.231/litellm',api_key='sk-6786') 
 
 
 print(client.chat.completions.create(model="ollama/mixtral", messages=[{"role":"user", "content":"What is the capital of the US?"}]))
